c4a5f1d68b04cb039ad4cb3f976c30fbd73f8846
EC2 Default User <ec2-user@ip-172-31-95-132.ec2.internal>
2020-04-23 04:22:44 +0000

    ec2 run

--- cronjob-myNodeApp.sh
+++ cronjob-myNodeApp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-PATH="~/.nvm/versions/node/v11.14.0/bin:$PATH"
+PATH=$PATH:$HOME/.local/bin:$HOME/bin:/home/ec2-user/.nvm/versions/node/v11.14.0/bin
 
-~/.nvm/versions/node/v11.14.0/bin/npm --prefix /home/ec2-user/StatusCheck run cy:run
+~/.nvm/versions/node/v11.14.0/bin/npm --prefix ~/StatusCheck run cy:run
